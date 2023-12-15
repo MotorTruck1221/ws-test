674cd4ebc130e5366674f67bca3e0e22a661d1ad
MotorTruck1221 <tuckerj0606@icloud.com>
2023-12-14 22:58:49 -0700

    Code cleanup

---
+++ build.js
@@ -0,0 +1,42 @@
+import { exec } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve();
+const rustServerPath = path.join(__dirname, 'Rust-server');
+const goServerPath = path.join(__dirname, 'go-test');
+const bunServerPath = path.join(__dirname, 'ws-bun-test');
+
+async function buildRust() {
+    fs.copyFile(path.join(__dirname, 'html', 'rust.html'), path.join(rustServerPath, 'rust.html'), (err) => {
+        if (err) throw err;
+        console.log('rust.html was copied to Rust-server');
+    });
+    exec('cargo build --release', { cwd: rustServerPath }, (err, stdout, stderr) => { console.log(stdout) });
+    console.log("Rust-server built");
+    return;
+}
+
+async function buildGo() {
+    exec('go build -o test', { cwd: goServerPath }, (err, stdout, stderr) => { console.log(stdout) });
+    console.log("go-server built");
+    return;
+}
+
+async function buildBun() {
+    fs.copyFile(path.join(__dirname, 'html', 'bun.html'), path.join(bunServerPath, 'index.html'), (err) => {
+        if (err) throw err;
+        console.log('bun.html was copied to ws-bun-test');
+    });
+    exec('bun build index.ts --outdir build/', { cwd: bunServerPath }, (err, stdout, stderr) => { console.log(stdout) });
+    console.log("ws-bun-test built");
+    return;
+}
+
+async function buildAll() {
+    await buildRust();
+    await buildGo();
+    await buildBun();
+    return;
+}
+
+buildAll();
---
+++ testAll.js
@@ -0,0 +1,53 @@
+//cp ./html/rust.html to ./Rust-server/ 
+
+import { exec } from 'child_process';
+import path from 'path';
+const __dirname = path.resolve();
+const rustServerPath = path.join(__dirname, 'Rust-server');
+const bunServerPath = path.join(__dirname, 'ws-bun-test');
+const goServerPath = path.join(__dirname, 'go-test');
+import rustTest from './tests/rust.js';
+import bunTest from './tests/bun.js';
+import goTest from './tests/go.js';
+
+async function testRust() {
+    const p = exec('./target/release/server', { cwd: rustServerPath }, (err, stdout, stderr) => {
+        console.log(stdout);
+    });
+    console.log("Rust-server started at: http://localhost:8000/rust.html");
+    await rustTest();
+    p.kill('SIGINT');
+    console.log("Rust-server stopped");
+    return;
+}
+
+async function testBun() {
+    const p = exec('bun build/index.js', { cwd: bunServerPath }, (err, stdout, stderr) => {
+        console.log(stdout);
+    });
+    console.log("ws-bun-test started at: http://localhost:3000/");
+    await bunTest();
+    p.kill('SIGINT');
+    console.log("ws-bun-test stopped");
+    return;
+}
+
+async function testGo() { 
+    const p = exec('./test', { cwd: goServerPath }, (err, stdout, stderr) => {
+        console.log(stdout);
+    });
+    console.log("go-server started at: http://localhost:3001/");
+    await goTest();
+    p.kill('SIGINT');
+    console.log("go-server stopped");
+    return;
+}
+
+async function startAll() {
+    await testRust();
+    await testBun();
+    await testGo();
+    process.exit(0);
+}
+
+startAll();
